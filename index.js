33912d7f7dca07c9b771bc464a74783e3209f6c5
unknown <sifatniloy18@gmail.com>
2022-12-01 11:13:12 -0600

    product load by sigle id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,12 +24,19 @@ async function run() {
             const products = await cursor.limit(6).toArray();
             res.send(products);
         })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
         app.get('/explore', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         })
+       
        
     }
     finally {
